46e3ed50ba965467fc079fdd57640d8abc761093
Derek Smith <derek.smith@naveego.com>
2020-09-02 20:29:31 -0400

    Updated styles and tried logo

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,22 +1,40 @@
 (function($) {
 
-    function getTheme() {
-        return localStorage.getItem('current-theme') ?? 'light';
+    var themes = {
+        light: {
+            id: 'light',
+            bodyClass: 'theme-light',
+            logo: '/assets/img/logo_white_bg.svg'
+        },
+        dark: {
+            id: 'dark',
+            bodyClass: 'theme-dark',
+            logo: '/assets/img/logo_dark_bg.svg'
+        }
     }
 
-    function setTheme(theme) {
-        localStorage.setItem('current-theme', theme);
-        $('body').removeClass('light').removeClass('dark').addClass(theme);
+    function getCurrentTheme() {
+        var t = localStorage.getItem('current-theme') ?? themes.light.id;
+        return themes[t];
+    }
+
+    function setTheme(t) {
+        localStorage.setItem('current-theme', t.id);
+        $('body').removeClass(themes.light.bodyClass).removeClass(themes.dark.bodyClass).addClass(t.bodyClass);
     }
 
     $(document).ready(function(){
-        $('body').addClass(getTheme());
+        $('body').addClass(getCurrentTheme().bodyClass);
 
         $('#theme-toggle').click(function(e) {
             e.preventDefault();
             
-            var nextTheme = (getTheme() === 'light') ? 'dark' : 'light';
+            var nextTheme = (getCurrentTheme() === themes.light) ? themes.dark : themes.light;
+
+            $('.logo img').attr('src', nextTheme.logo);
+
             setTheme(nextTheme);
         });
     });
+    
 })(jQuery);
